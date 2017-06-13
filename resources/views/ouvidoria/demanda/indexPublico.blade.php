991bf2dea647c45830cef79d47d98734e0881eb0
Fabio <fabio@example.com>
2017-06-13 12:30:07 -0300

    atualização

--- resources/views/ouvidoria/demanda/indexPublico.blade.php
+++ resources/views/ouvidoria/demanda/indexPublico.blade.php
@@ -37,7 +37,7 @@
 
 <div class="loginColumns animated fadeInDown">
     <div class="row">
-        <div class="col-sm-2 col-md-5 col-md-offset-1">
+        <div class="col-sm-1 col-md-5 col-md-offset-1">
             <div class="ibox-content">
                 <p>
                     <a href="{{ route('buscarDemanda')  }}" title="cadastro da manifestação">
@@ -46,7 +46,7 @@
                 </p>
             </div>
         </div>
-        <div class="col-sm-2 col-md-5">
+        <div class="col-sm-1 col-md-5">
             <div class="ibox-content">
                 <p>
                     <a href="{{ route('createPublico')  }}" title="consultar da manifestação">
