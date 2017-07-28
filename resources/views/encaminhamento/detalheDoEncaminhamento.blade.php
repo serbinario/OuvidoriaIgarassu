632456551f966846ee1ed7c57652df56b391db25
Fabio <fabio@example.com>
2017-07-28 08:02:26 -0300

    atualização

--- resources/views/encaminhamento/detalheDoEncaminhamento.blade.php
+++ resources/views/encaminhamento/detalheDoEncaminhamento.blade.php
@@ -267,7 +267,7 @@
                                                         <td style="width : 50%">{{ $detalheEncaminhamento->previsao }}</td>
                                                         <td>
                                                             @role('ouvidoria|admin')
-                                                                @if(!$detalheEncaminhamento->prazo_solucao &&
+                                                                @if(!$detalheEncaminhamento->prazo_solucao ||
                                                                 (isset($encaminhamentoAnterior->prazo_solucao) && !$encaminhamentoAnterior->prazo_solucao) &&
                                                                 $detalheEncaminhamento->status_id != '6')
                                                                     <button type="button" data-toggle="modal"
